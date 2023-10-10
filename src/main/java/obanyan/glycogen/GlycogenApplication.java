33ca6e7c8b185e284767ecf5a78de9588437200b
sho037 <103867061+sho037@users.noreply.github.com>
2023-10-10 14:18:55 +0900

    First Commit

---
+++ src/main/java/obanyan/glycogen/GlycogenApplication.java
@@ -0,0 +1,13 @@
+package obanyan.glycogen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlycogenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GlycogenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/obanyan/glycogen/GlycogenApplicationTests.java
@@ -0,0 +1,13 @@
+package obanyan.glycogen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GlycogenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
