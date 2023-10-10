e4b1603e3b944a4828cdf063db88862aa21bf6d5
e1b21115 <119831972+e1b21115@users.noreply.github.com>
2023-10-10 16:37:57 +0900

    sample37.htmlへのリンクの追加及びsample37からのログアウトの追加

--- src/main/java/obanyan/glycogen/controller/Sample31Controller.java
+++ src/main/java/obanyan/glycogen/controller/Sample31Controller.java
@@ -12,30 +12,39 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 @RequestMapping("/sample3")
 public class Sample31Controller {
-    
-    @GetMapping("/step1")
-    public String sample31 (){
-        return "sample31.html";
-    }
 
-    @GetMapping("/step2")
-    public String sample32(ModelMap model, Principal prin){
-        String login_user = prin.getName();
-        model.addAttribute("login_user", login_user);
-        return "sample31.html";
-    }
+  @GetMapping("step1")
+  public String sample31() {
+    return "sample31.html";
+  }
 
-    @GetMapping("step3")
-    public String sample33() {
-      return "sample33.html";
-    }
+  @GetMapping("step2")
+  public String sample32(ModelMap model, Principal prin) {
+    String login_user = prin.getName();
+    model.addAttribute("login_user", login_user);
+    return "sample31.html";
+  }
 
-    @PostMapping("step6")
-    public String sample36(@RequestParam Integer hiku1, @RequestParam Integer hiku2, ModelMap model) {
-        int kekka = hiku1 - hiku2;
-        model.addAttribute("hikukekka", kekka);
-        return "sample33.html";
-    }
+  @GetMapping("step3")
+  public String sample33() {
+    return "sample33.html";
+  }
 
+  @PostMapping("step6")
+  public String sample36(@RequestParam Integer hiku1, @RequestParam Integer hiku2, ModelMap model) {
+    int kekka = hiku1 - hiku2;
+    model.addAttribute("hikukekka", kekka);
+    return "sample33.html";
+  }
+
+  @GetMapping("step7")
+  public String sample37() {
+    return "sample37.html";
+  }
+
+  @GetMapping("logout")
+  public String logout() {
+    return "sample31.html";
+  }
 
 }
