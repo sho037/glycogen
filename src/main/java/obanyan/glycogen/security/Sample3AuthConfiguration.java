77ee2bea03efaf9d32b8c2fd4e42ca8e891fefc0
e1b21115 <119831972+e1b21115@users.noreply.github.com>
2023-10-10 15:42:23 +0900

    認可機能の追加

--- src/main/java/obanyan/glycogen/security/Sample3AuthConfiguration.java
+++ src/main/java/obanyan/glycogen/security/Sample3AuthConfiguration.java
@@ -36,4 +36,25 @@ public class Sample3AuthConfiguration {
     // 生成したユーザをImMemoryUserDetailsManagerに渡す（いくつでも良い）
     return new InMemoryUserDetailsManager(user, admin);
   }
+
+  /**
+   * 認可処理に関する設定（認証されたユーザがどこにアクセスできるか）
+   *
+   * @param http
+   * @return
+   * @throws Exception
+   */
+  @Bean
+  public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    http.formLogin(login -> login
+        .permitAll())
+        .logout(logout -> logout
+            .logoutUrl("/logout")
+            .logoutSuccessUrl("/")) // ログアウト後に / にリダイレクト
+        .authorizeHttpRequests(authz -> authz
+            .requestMatchers(AntPathRequestMatcher.antMatcher("/sample3/**")).authenticated() // /sample3/以下は認証済みであること
+            .requestMatchers(AntPathRequestMatcher.antMatcher("/**")).permitAll()); // それ以外は全員アクセス可能
+    return http.build();
+  }
+
 }
