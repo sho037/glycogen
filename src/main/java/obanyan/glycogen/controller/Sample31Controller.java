ec97b5946606dd794982a5b489840e6fb63888f1
e1b21072 <e1b21072@oit.ac.jp>
2023-10-10 16:23:59 +0900

    sample3-5の完了

--- src/main/java/obanyan/glycogen/controller/Sample31Controller.java
+++ src/main/java/obanyan/glycogen/controller/Sample31Controller.java
@@ -5,7 +5,9 @@ import java.security.Principal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/sample3")
@@ -26,6 +28,13 @@ public class Sample31Controller {
     @GetMapping("step3")
     public String sample33() {
       return "sample33.html";
+    }
+
+    @PostMapping("step6")
+    public String sample36(@RequestParam Integer hiku1, @RequestParam Integer hiku2, ModelMap model) {
+        int kekka = hiku1 - hiku2;
+        model.addAttribute("hikukekka", kekka);
+        return "sample33.html";
     }
 
 
